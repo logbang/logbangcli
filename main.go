5a69fbf60766b3f76e6fff5a711ef0f650bb16bc
Georges Haidar <ghaidar0@gmail.com>
2017-12-21 11:36:20 +0000

    colorise output

--- main.go
+++ main.go
@@ -14,7 +14,13 @@ import (
 )
 
 var blue = color.New(color.FgBlue).SprintFunc()
-var bgblue = color.New(color.BgBlue).SprintfFunc()
+var ctxPre = color.New(color.BgCyan).Sprint(" ")
+var unparsedPre = color.New(color.BgYellow, color.FgBlack).Sprint("SKP")
+var errorPre = color.New(color.BgRed, color.FgBlack).Sprint("ERR")
+var warningPre = color.New(color.BgYellow, color.FgBlack).Sprint("WRN")
+var infoPre = color.New(color.BgGreen, color.FgBlack).Sprint("INF")
+var debugPre = color.New(color.BgBlue, color.FgBlack).Sprint("DBG")
+var levelPre = []string{errorPre, warningPre, infoPre, debugPre}
 
 // Context holds additional info in a log payload about the log event
 type Context map[string]interface{}
@@ -23,7 +29,7 @@ func (c Context) String() string {
 	pairs := make([]string, len(c))
 	i := 0
 	for k, v := range c {
-		pairs[i] = fmt.Sprintf("\t%s %s = %v", bgblue(" "), k, v)
+		pairs[i] = fmt.Sprintf("\t%s %s = %v", ctxPre, k, v)
 		i++
 	}
 	sort.Strings(pairs)
@@ -32,29 +38,34 @@ func (c Context) String() string {
 
 // Payload is a log event
 type Payload struct {
-	Format  string  `json:"__f"`
-	Message string  `json:"msg"`
-	Time    int64   `json:"time"`
-	Level   int64   `json:"level"`
-	Logger  string  `json:"logger"`
-	Context Context `json:"ctx"`
+	Format   string  `json:"__f"`
+	Language string  `json:"__l"`
+	Message  string  `json:"msg"`
+	Time     int64   `json:"time"`
+	Level    int64   `json:"level"`
+	Logger   string  `json:"logger"`
+	Context  Context `json:"ctx"`
 }
 
 func (p *Payload) String() string {
 	sec := p.Time / 1000
 	nsec := p.Time % 1000 * 1e6
 	ts := time.Unix(sec, nsec).Format(time.RFC3339Nano)
-	return fmt.Sprintf("[%s] %s -> %s\n%s", ts, blue(p.Logger), p.Message, p.Context)
+	return strings.TrimSpace(
+		fmt.Sprintf(
+			"%s [%s] %s -> %s\n%s",
+			levelPre[p.Level], ts, blue(p.Logger), p.Message, p.Context,
+		),
+	)
 }
 
 func parseLine(line []byte) {
-	p := Payload{}
-	err := json.Unmarshal(line, &p)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "[log!] error parsing log line: %s", err)
-		return
+	p := &Payload{}
+	if err := json.Unmarshal(line, p); err == nil {
+		fmt.Println(p)
+	} else {
+		fmt.Printf("%s %s\n", unparsedPre, line)
 	}
-	fmt.Println(&p)
 }
 
 func main() {
